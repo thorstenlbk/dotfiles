d8a2aad887cad6afc55cae03c09bf7c5a726b8fb
Thorsten Lübkemann <kontakt@luebkemann-design.de>
2024-12-15 16:40:33 +0100

    [Install] Create symbolic links, forcefully replacing any existing links or files.

--- install.zsh
+++ install.zsh
@@ -3,9 +3,10 @@
 DOTFILES=`pwd`
 
 cd ~
-ln -s ${DOTFILES}/.vimrc
-ln -s ${DOTFILES}/.vim
-ln -s ${DOTFILES}/.tmux
+# Create symbolic links for the configuration files and directories, forcefully replacing any existing links or files.
+ln -sf ${DOTFILES}/.vimrc ~
+ln -sf ${DOTFILES}/.vim ~
+ln -sf ${DOTFILES}/.tmux ~
 
 mkdir -p ~/.config/nvim
 ln -s ${DOTFILES}/.vimrc ~/.config/nvim/init.vim
